4edc084e1d07badd50f3559a1357962ab95de577
Kartik Arora <kartikaroraofficial01@gmail.com>
2024-03-19 03:20:15 +0530

    test

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,10 +1,12 @@
 'use client';
 import { XMarkIcon } from '@heroicons/react/20/solid'
 import { bannerDes } from '@/content/banner';
+import { useState } from 'react';
 
 export default function Banner() {
+  const [display, setDisplay] = useState<boolean>(true);
   return (
-    <div id='banner' className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+   (display)? <div id='banner' className="relative isolate flex items-center gap-x-6 overflow-hidden bg-gray-50 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
       <div
         className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
         aria-hidden="true"
@@ -45,11 +47,11 @@ export default function Banner() {
         </a>
       </div>
       <div className="flex flex-1 justify-end">
-        <button type="button" className="-m-3 p-3 focus-visible:outline-offset-[-4px]" onClick={() => {let banner = document.getElementById('banner'); (banner)? banner.style.display = 'none': null}}>
+        <button type="button" className="-m-3 p-3 focus-visible:outline-offset-[-4px]" onClick={() => {setDisplay(false)}}>
           <span className="sr-only">Dismiss</span>
           <XMarkIcon className="h-5 w-5 text-gray-900" aria-hidden="true" />
         </button>
       </div>
-    </div>
+    </div> : <div></div>
   )
 }
